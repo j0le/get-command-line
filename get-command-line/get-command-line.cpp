d96646118eefe391d82590fcbd79ae922fdbb048
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-06-12 18:18:19 +0200

    Write as UTF-8, if redirected
    If stdout is redirected to a file/device that is not a console screen
    buffer, than convert the commandline from UTF-16 to UTF-8 and write the
    bytes to the file or device or pipe or whatever via the std out handle.

--- get-command-line/get-command-line.cpp
+++ get-command-line/get-command-line.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <string>
 #include <type_traits>
+#include <nowide/convert.hpp>
 
 static_assert(true == TRUE);
 static_assert(false == FALSE);
@@ -59,7 +60,42 @@ int main()
         }
     }
     else {
+        auto cmdline_utf8_str = ::nowide::narrow(cmdline_wstr);
+        static_assert(::std::is_same_v<decltype(cmdline_utf8_str), ::std::string>);
+        static_assert(sizeof(::std::string::value_type) == 1);
 
+        auto cmdline_utf8_cstr = cmdline_utf8_str.c_str();
+        const DWORD absolute_number_of_bytes_to_write = cmdline_utf8_str.length(); // without terminating null character
+        DWORD absolute_number_of_bytes_written = 0;
+        DWORD number_of_bytes_written = 0;
+        bool success = true;
+        while
+        (
+            absolute_number_of_bytes_written < absolute_number_of_bytes_to_write
+            &&
+            (
+                success = WriteFile(
+                    /*[in]                HANDLE       hFile,                  */ std_out_handle,
+                    /*[in]                LPCVOID      lpBuffer,               */ &cmdline_utf8_cstr[absolute_number_of_bytes_written],
+                    /*[in]                DWORD        nNumberOfBytesToWrite,  */ absolute_number_of_bytes_to_write - absolute_number_of_bytes_written,
+                    /*[out, optional]     LPDWORD      lpNumberOfBytesWritten, */ &number_of_bytes_written,
+                    /*[in, out, optional] LPOVERLAPPED lpOverlapped            */ nullptr
+                )
+            )
+            &&
+            number_of_bytes_written > 0
+        ){
+            absolute_number_of_bytes_written += number_of_bytes_written;
+        }
+
+        if
+        (
+            !success
+            ||
+            absolute_number_of_bytes_written != absolute_number_of_bytes_to_write
+        ){
+            return 1;
+        }
     }
 
     return 0;
