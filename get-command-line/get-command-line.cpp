9cc2bc5ab3969de1e5e7c404f7eb8b76ca0e7c10
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-06-12 16:16:38 +0200

    Get command line as UTF-16

--- get-command-line/get-command-line.cpp
+++ get-command-line/get-command-line.cpp
@@ -1,6 +1,14 @@
-
+#include <Windows.h>
+#include <string>
 
 int main()
 {
+    ::HANDLE std_out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (std_out_handle == INVALID_HANDLE_VALUE)
+        return 1;
+
+    ::std::wstring cmdline_wstr(::GetCommandLineW());
+    
+
     return 0;
 }
